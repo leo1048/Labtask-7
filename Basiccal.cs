8aa4220105383e0c0d921cb5cf825846f1723d8f
leo1048 <74731859+leo1048@users.noreply.github.com>
2020-11-25 12:01:20 +0600

    Add files via upload

---
+++ Basiccal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+	class Basiccal
+	{
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;                  
+            string operation;
+            int answer;
+
+            Console.Write("Enter first number equation: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number equation: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Select operation ( x , / , +, -) ");
+            operation = Console.ReadLine();
+
+            if (operation == "x")
+            {
+                answer = firstNum * secondNum;
+                Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                answer = firstNum / secondNum;
+                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            
+            else if (operation == "+")
+            {
+                answer = firstNum + secondNum;
+                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "-")
+            {
+                answer = firstNum - secondNum;
+                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");     
+                Console.ReadLine();
+            }
+        }
+    }
+}
+}
